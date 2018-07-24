1d70339b85b96ecde574a2b7d098b3d09fc14136
NikAlexSer <alexandernikser@gmail.com>
2018-07-24 16:45:38 +0500

    feat: add prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 let gulp = require('gulp');
 let sass = require('gulp-sass');
+let prefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   gulp.src('./public/stylesheets/*.scss')
       .pipe(sass().on('error', sass.logError))
+      .pipe(prefixer())
       .pipe(gulp.dest('./public/stylesheets/'));
 });
 
